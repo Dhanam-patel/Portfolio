7e30d189dd6b62c2e4f3fd3c3c7397e4c2e2c74c
Dhanam-patel <dhanampatel2005@gmail.com>
2026-04-16 21:24:26 +0530

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
-        {process.env.NODE_ENV === 'production' && <Analytics />}
+        <Analytics />
       </body>
     </html>
   )
